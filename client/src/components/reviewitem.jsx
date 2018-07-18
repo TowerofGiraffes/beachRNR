822b336a2ec85f9f102ab00e22f5031d082df096
Yang Chen <y.chen.pf@gmail.com>
2018-07-17 19:46:55 -0700

    update style component

--- client/src/components/reviewitem.jsx
+++ client/src/components/reviewitem.jsx
@@ -9,15 +9,25 @@ const ReviewItemAvatar = styled.img`
   border-radius: 50%;
 `;
 
+const ReviewContent = styled.div`
+  clear: both;
+  height: 4.8em;
+  line-height: 1.2em;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
 const ReviewItem = (props) => (
   <div>
     <ReviewItemAvatar src={props.user_avatar} />
     <span>{props.user_name}</span>
     <br></br>
     <span>{props.review_date}</span>
-    <p style = {{'clear': 'both'}}>
+    <ReviewContent>
+      <p>
       {props.review_content}
-    </p>
+      </p>
+    </ReviewContent>
   </div>
 );
 
