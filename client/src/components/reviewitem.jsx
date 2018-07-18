b6ce81dba164a0cb1fb87c7aa320646ecd5b03eb
Yang Chen <y.chen.pf@gmail.com>
2018-07-17 21:02:13 -0700

    style review content

--- client/src/components/reviewitem.jsx
+++ client/src/components/reviewitem.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
+const ReviewItemDiv = styled.div`
+  margin-top: 1em;
+  margin-bottom: 1em;
+  border-bottom-style: solid;
+  border-bottom-color: grey;
+  border-width: 1px;
+`;
+
 const ReviewItemAvatar = styled.img`
-  width: 50px;
-  height: 50px;
+  width: 40px;
+  height: 40px;
   float: left;
   border-radius: 50%;
 `;
@@ -17,18 +25,23 @@ const ReviewContent = styled.div`
   text-overflow: ellipsis;
 `;
 
+const ReviewPerson = styled.span`
+  margin: 0.8em;
+  line-height: 1.3em;
+`;
+
 const ReviewItem = (props) => (
-  <div>
+  <ReviewItemDiv>
     <ReviewItemAvatar src={props.user_avatar} />
-    <span>{props.user_name}</span>
+    <ReviewPerson>{props.user_name}</ReviewPerson>
     <br></br>
-    <span>{props.review_date}</span>
+    <ReviewPerson>{props.review_date}</ReviewPerson>
     <ReviewContent>
       <p>
       {props.review_content}
       </p>
     </ReviewContent>
-  </div>
+  </ReviewItemDiv>
 );
 
 
