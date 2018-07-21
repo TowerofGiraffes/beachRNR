807da481b7e0d447351bfd4f8556156aa044f5ce
Zoey Ikemoto <zmi384@gmail.com>
2018-07-21 14:44:29 -0700

    styled details component: MVP build

--- client/src/components/Details/ListingDetails.jsx
+++ client/src/components/Details/ListingDetails.jsx
@@ -10,15 +10,18 @@ const MainImage = styled.div`
 const MainContainer = styled.div`
     width: 60%;
     margin: 0 auto;
-    border-bottom-style: solid;
-    border-bottom-color: grey;
-    border-width: 1px;
-    padding-top: 20px;
-    padding-bottom: 50px;
+`;
+
+const Title = styled.h1`
 `;
 
 const Description = styled.div`
   width: 80%;
+  border-bottom-style: solid;
+  border-bottom-color: grey;
+  border-width: 1px;
+  padding-top: 20px;
+  padding-bottom: 50px;
 `;
 
 class ListingDetails extends React.Component {
@@ -52,12 +55,58 @@ class ListingDetails extends React.Component {
 
         </MainImage>
         <MainContainer>
-          <h1>{data.unitName}</h1>
+          <Title>{data.unitName}</Title>
           <h3>{data.city}, {data.state}</h3>
           <h4>{data.beds} Bedroom  {data.property_type}</h4>
           <h4>{data.unitPrice} {data.priceModifier}</h4>
           <Description>
             <p>{data.description_short}</p>
+          </Description>
+          <Description>
+            <h3>Amenities</h3>
+            <ul>
+              <li>
+                Wifi
+              </li>
+              <li>
+                Cable TV
+              </li>
+              <li>
+                Air conditioning
+              </li>
+              <li>
+                Laptop friendly workspace
+              </li>
+              <li>
+                TV
+              </li>
+              <li>
+                Iron
+              </li>
+            </ul>
+          </Description>
+          <Description>
+            <h3>House Rules</h3>
+            <ul>
+              <li>
+                Not safe or suitable for children (0-12 years)
+              </li>
+              <li>
+                No smoking
+              </li>
+              <li>
+                Not suitable for pets
+              </li>
+              <li>
+                No parties or events
+              </li>
+              <li>
+                Check-in is anytime after 4PM
+              </li>
+              <li>
+                Check out by 11AM
+              </li>
+            </ul>
           </Description>
         </MainContainer>
       </div>
