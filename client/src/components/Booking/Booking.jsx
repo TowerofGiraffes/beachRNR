d112a601b9718304bbd88af509b69f58092e0bb7
joseph <jbgomez88@gmail.com>
2018-07-21 09:42:23 -0700

    Add Booking component skeleton

---
+++ client/src/components/Booking/Booking.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+
+class Booking extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Card>
+        <Card.Content>
+          <Card.Header></Card.Header>
+        </Card.Content>
+      </Card>
+    )
+  }
+}
+
+export default Booking;
